3e9c6334ccbfa7d77061d8b805fef3eb2b68b376
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-03-08 18:15:41 +0300

    made changes

--- lib/profileclass.dart
+++ lib/profileclass.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:cookbook1/Homepage.dart';
-import 'package:firebase_auth/firebase_auth.dart';
- class Profile extends StatefulWidget {
-   const Profile({Key? key}) : super(key: key);
-
-   @override
-   State<Profile> createState() => _ProfileState();
- }
-
- class _ProfileState extends State<Profile> {
-   @override
-   Widget build(BuildContext context) {
-     return Container(
-      child: SingleChildScrollView(
-       child: Column(
-        children: [
-          FutureBuilder(
-            future: Provider.of(context).a,
-              builder: ),
-        ],
-       ),
-      ),
-     );
-   }
- }
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:cookbook1/Homepage.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+//  class Profile extends StatefulWidget {
+//    const Profile({Key? key}) : super(key: key);
+//
+//    @override
+//    State<Profile> createState() => _ProfileState();
+//  }
+//
+//  class _ProfileState extends State<Profile> {
+//    @override
+//    Widget build(BuildContext context) {
+//      return Container(
+//       child: SingleChildScrollView(
+//        child: Column(
+//         children: [
+//           FutureBuilder(
+//             future: Provider.of(context).a,
+//               builder: (BuildContext conext)),
+//         ],
+//        ),
+//       ),
+//      );
+//    }
+//  }
